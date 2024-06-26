1074f4020d091fadd78acc2fb8d44f841e8a2ca6
tenessy_de-faria <tenessy.de-faria@knap.fr>
2024-06-26 14:16:33 -0400

    Merge des deux approches de désorbition (énergétque et PFD)

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
+from datetime import datetime
 from Astraios import *
-import time
 
 # ---------------------Création d'orbite---------------------
 
-perigee = 200 * 10**3   # Altitude du périgée [m]
-apogee = 600 * 10**3    # Altitude de l'apogée [m]
-inclinaison = 0        # Inclinaison de l'orbite [°]
+altitude = 150 * 10 ** 3  # Altitude du périgée [m]
+inclinaison = 30  # Inclinaison de l'orbite [°]
 
-orbite_initiale = Orbite(perigee, apogee, inclinaison )
-
-
-# ---------------------Manoeuvre orbitale---------------------
-delta_v = 1000  # Changement de vitesse en m/s
-direction = Type_manoeuvre.prograde  # Direction de la manoeuvre ('prograde', 'retrograde' ou 'radiale')
-position_manoeuvre = Position_manoeuvre.apogee  # Position de la manoeuvre ('perigee' ou 'apogee')
-
-nouvelle_orbite = orbite_initiale.manoeuvre(delta_v, direction, position_manoeuvre)
-
-# Affichage du nouvel orbite
-nouvelle_orbite.plot_orbit()
-
-
-#-----------------------Desorbitation------------------------
-# Création d'un satellite et de l'atmosphere
-spoutnik = Satellite(100, 0.5, 2)
+date = datetime(2021, 3, 28)
+champ_magnetique = champ_mag(date)
 atmosphere_terrestre = Atmosphere()
 
-start = time.time()
-# Affichage du temps de vie estimé
-orbite_initiale.desorbitation(spoutnik, orbite_initiale, position_manoeuvre, atmosphere_terrestre)
-print(f"Duree de calcul : {time.time() - start}")
+cable_mag = cable(5000, 185, 45)
+satMag = satellite_magnetique(1300, 0.5, cable_mag)
+orbite = Orbite(altitude, inclinaison, 300)
+orbite.desorbitation_PFD(satMag, atmosphere_terrestre, champ_magnetique)
